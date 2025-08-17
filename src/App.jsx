22ece3f82965d8a732b1d17d387da51974c49c4a
Div-30 <divinpaul88@gmail.com>
2025-08-17 11:14:57 +0200

    Resolved conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import FacultyDirectory from "./FacultyDirectory";
-import FacultyServices from "./FacultyServices";
-import Footer from "./Footer";
-import HeroSection from "./HeroSection";
-import QuickLinks from "./QuicksLink";
-import ResourceCategories from "./ResourceCategories";
-
+import React from 'react';
+import FacultyDirectory from './FacultyDirectory';
+import FacultyServices from './FacultyServices';
+import Footer from './Footer';
+import HeroSection from './HeroSection';
+import QuickLinks from './QuickLinks';
+import ResourceCategories from './ResourceCategories';
 
 function App() {
-
   return (
-<div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white">
       <HeroSection />
       <FacultyServices />
       <ResourceCategories />
@@ -17,7 +16,7 @@ function App() {
       <QuickLinks />
       <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
